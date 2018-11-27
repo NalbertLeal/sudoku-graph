4e7d98deaae990141cd4441bc66de7721852d94c
Nalbert Gabriel Melo Leal <nalbertg@outlook.com>
2018-11-27 18:33:11 -0300

    utils/list_utils dont need try/except

--- util/list_utils.py
+++ util/list_utils.py
@@ -1,9 +1,5 @@
 def list_to_str(the_list):
-  try:
-    str_result = ''
-    for element in the_list:
-      str_result += str(element)
-    return str_result
-  except Exception as e:
-    print(the_list)
-    return the_list
+  str_result = ''
+  for element in the_list:
+    str_result += str(element)
+  return str_result
